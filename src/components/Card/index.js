4b413171824f0d04bc6c1a6e6b2a58c7cbf23089
rodrigovallades <rodrigo.vallades@gmail.com>
2018-05-08 21:35:27 -0300

    Styling product card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -15,10 +15,13 @@ export class Card {
   render() {
     return `
       <div class='product'>
-        <div class='product__imageName'><img src='${this.imageName}' /></div>
+        <div class='product__image'><img src='${this.imageName}' /></div>
         <div class='product__name'>${this.name}</div>
-        <div class='product__price'>Por: <span>${this.name}</span></div>
-        <div class='product__paymentConditions'>${this.paymentConditions}</div>
+        <div class='product__price'>Por: <span>${this.price}</span></div>
+        <div class='product__conditions'>${this.paymentConditions}</div>
+        <div class='product__actions'>
+          <div class='product__actions--add'>Adicionar ao carrinho <i class="material-icons">add_shopping_cart</i></div>
+        </div>
       </div>
     `
   }
